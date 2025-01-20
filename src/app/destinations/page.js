438c8cd812a4ea9a2d1724cae251cb38697d0972
Udo <princeinme128@gmail.com>
2025-01-20 14:11:15 +0100

    updates

--- src/app/destinations/page.js
+++ src/app/destinations/page.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import Image from 'next/image';
-import { getAllDestinations } from '../../../database/destination';
-const destination = getAllDestinations();
+// import React from 'react';
+// import Image from 'next/image';
+// import { getAllDestinations } from '../../../database/destination';
+// const destination = getAllDestinations();
 
-function Page({ destination }) { 
+// function Page({ destination }) { 
     
-    // Set default value to empty array
-    return (
-        <div>
-            <h1>Destinations</h1>
+//     // Set default value to empty array
+//     return (
+//         <div>
+//             <h1>Destinations</h1>
          
-        </div>
-    );
+//         </div>
+//     );
+// }
+
+// export default Page;
+
+import React from 'react'
+
+function page() {
+  return (
+    <div>page</div>
+  )
 }
 
-export default Page;
+export default page
